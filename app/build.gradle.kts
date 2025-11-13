9e3c46dbf3c3abed4f67e4616003b2e8a1b600d4
ray <haozanlawyer@gmail.com>
2025-11-13 14:31:57 +0800

    Update version to 1.1.1 and increment version code to 3 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.jingbao.helper"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
